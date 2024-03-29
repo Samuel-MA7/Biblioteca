80c7478628a9f60926a682ea0d6748b5b6f29a2d
Samuel-MA7 <s4muelm0ura@gmail.com>
2024-03-29 20:39:40 -0300

    nodemon and first routes

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ import http from 'http'
 //server's port
 const PORT = 3000
 
+//server's route
+const routes = {
+    '/': 'Node.js and Express curse',
+    '/books':'You have entered in books route',
+    '/authors':'You have entered in authors route'
+}
+
 //create a server
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type':'text/plan'})
-    res.end("Node.js curse.")
+    res.end(routes[req.url])
 })
 
 server.listen(PORT, () => {
